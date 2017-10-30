d7c398843f0be8d2446d54bbc1bd3cbb06570cef
Oleg Kelin <oleg.kelin@kodep.ru>
2017-10-31 02:20:39 +0300

    IR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MsApp::Application.config.secret_key_base = 'ef1e2f0c42dea8aac404b400de51a77e6a3f7e949eb203d841ae7079fb0df6e890023e93945eef1b44dd8cc2640c5e23ef39d0eaf9c52b6533774d92afb00ef8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MsApp::Application.config.session_store :cookie_store, key: '_ms_app_session'
